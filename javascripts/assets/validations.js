4e8e62d4adc0dcd16388e733f915f0ce63736d49
iMykhailychenko <igor.c.m@ukr.net>
2021-04-18 19:52:18 +0300

    init

--- javascripts/assets/validations.js
+++ javascripts/assets/validations.js
@@ -14,106 +14,116 @@ export default class Validations {
         return false;
     }
 
-    jump = () => ({
-        cell() {
-            const selected = this.lake.services.getCheckedCells();
-            const frogs = selected.filter((cell) => cell.frog);
+    jump() {
+        return {
+            cell() {
+                const selected = this.lake.services.getCheckedCells();
+                const frogs = selected.filter((cell) => cell.frog);
 
-            // validate selected cells
-            if (selected.length < 2) {
+                // validate selected cells
+                if (selected.length < 2) {
+                    alert(
+                        'You have selected less than 2 fields! You should select the frog, the empty field and click the jump button.'
+                    );
+                    return true;
+                }
+
+                // validate selected frogs
+                if (frogs.length >= 2) {
+                    alert(
+                        'You have selected 2 frogs! You should select the frog, the empty field and click the jump button.'
+                    );
+                    return true;
+                }
+                if (!frogs.length) {
+                    alert(
+                        'You have not selected any frog! You should select the frog, the empty field and click the jump button.'
+                    );
+                    return true;
+                }
+
+                return false;
+            },
+
+            directions({ newPosition, oldPosition }) {
+                const frog = this.lake.services
+                    .getCheckedCells()
+                    .filter((cell) => cell.frog)[0].frog;
+
+                // horizontal jump
+                if (newPosition.row === oldPosition.row) {
+                    const dif = Math.abs(newPosition.cell - oldPosition.cell);
+
+                    if (dif > frog.distance) {
+                        alert(
+                            `This frog can only jump ${frog.distance} fields`
+                        );
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                // vertical jump
+                if (newPosition.cell === oldPosition.cell) {
+                    const dif = Math.abs(newPosition.row - oldPosition.row);
+
+                    if (dif > frog.distance) {
+                        alert(
+                            `This frog can only jump ${frog.distance} fields`
+                        );
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                // diagonal jump
+                // calse 1
+                if (
+                    newPosition.cell - newPosition.row ===
+                    oldPosition.cell - oldPosition.row
+                ) {
+                    const distance =
+                        (Math.abs(oldPosition.cell - newPosition.cell) +
+                            Math.abs(oldPosition.row - newPosition.row)) /
+                        2;
+
+                    if (distance > frog.distance) {
+                        alert(
+                            `This frog can only jump ${frog.distance} fields`
+                        );
+                        return true;
+                    }
+
+                    return false;
+                }
+                // calse 2
+                if (
+                    newPosition.cell + newPosition.row ===
+                    oldPosition.cell + oldPosition.row
+                ) {
+                    const distance =
+                        (Math.abs(oldPosition.cell - newPosition.cell) +
+                            Math.abs(oldPosition.row - newPosition.row)) /
+                        2;
+                    if (distance > frog.distance) {
+                        alert(
+                            `This frog can only jump ${frog.distance} fields`
+                        );
+                        return true;
+                    }
+
+                    return false;
+                }
+
                 alert(
-                    'You have selected less than 2 fields! You should select the frog, the empty field and click the jump button.'
+                    `This frog can only jump ${frog.distance} fields in vertical, horizontal and diagonal orientations.`
                 );
                 return true;
-            }
-
-            // validate selected frogs
-            if (frogs.length >= 2) {
-                alert(
-                    'You have selected 2 frogs! You should select the frog, the empty field and click the jump button.'
-                );
-                return true;
-            }
-            if (!frogs.length) {
-                alert(
-                    'You have not selected any frog! You should select the frog, the empty field and click the jump button.'
-                );
-                return true;
-            }
-
-            return false;
-        },
-
-        directions({ newPosition, oldPosition }) {
-            const frog = this.lake.services
-                .getCheckedCells()
-                .filter((cell) => cell.frog)[0].frog;
-
-            // horizontal jump
-            if (newPosition.row === oldPosition.row) {
-                const dif = Math.abs(newPosition.cell - oldPosition.cell);
-
-                if (dif > frog.distance) {
-                    alert(`This frog can only jump ${frog.distance} fields`);
-                    return true;
-                }
-
-                return false;
-            }
-
-            // vertical jump
-            if (newPosition.cell === oldPosition.cell) {
-                const dif = Math.abs(newPosition.row - oldPosition.row);
-
-                if (dif > frog.distance) {
-                    alert(`This frog can only jump ${frog.distance} fields`);
-                    return true;
-                }
-
-                return false;
-            }
-
-            // diagonal jump
-            // calse 1
-            if (
-                newPosition.cell - newPosition.row ===
-                oldPosition.cell - oldPosition.row
-            ) {
-                const distance =
-                    (Math.abs(oldPosition.cell - newPosition.cell) +
-                        Math.abs(oldPosition.row - newPosition.row)) /
-                    2;
-
-                if (distance > frog.distance) {
-                    alert(`This frog can only jump ${frog.distance} fields`);
-                    return true;
-                }
-
-                return false;
-            }
-            // calse 2
-            if (
-                newPosition.cell + newPosition.row ===
-                oldPosition.cell + oldPosition.row
-            ) {
-                const distance =
-                    (Math.abs(oldPosition.cell - newPosition.cell) +
-                        Math.abs(oldPosition.row - newPosition.row)) /
-                    2;
-                if (distance > frog.distance) {
-                    alert(`This frog can only jump ${frog.distance} fields`);
-                    return true;
-                }
-
-                return false;
-            }
-
-            alert(
-                `This frog can only jump ${frog.distance} fields in vertical, horizontal and diagonal orientations.`
-            );
-            return true;
-        },
-    });
+            },
+        };
+    }
 
     reproduce() {
         const selected = this.lake.services.getCheckedCells();
